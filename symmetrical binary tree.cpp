949fd7900a9404a8c4df9357845e4ae0d1de6702
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-07 21:51:42 +0530

    Create symmetrical binary tree.cpp

---
+++ symmetrical binary tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool issym(TreeNode* left,TreeNode* right)
+    {
+        if(left==NULL || right==NULL)
+        {
+            return left==right;
+        }
+        if(left->val!=right->val) return false;
+        return issym(left->left,right->right)&& issym(left->right,right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return root==NULL || issym(root->left,root->right);
+    }
+};
