44ccf071a146e378ccd81ef50a3525ae6277550f
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-11 19:11:05 +0530

    Create armstrong_number.cpp

---
+++ armstrong_number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        int dup=n;
+        int rev=0;
+        int sum=0;
+        int cnt=0;
+        int temp=n;
+        while(temp>0)
+        {
+            cnt++;
+            temp=temp/10;
+        }
+        while(n>0)
+        {
+            int ld=n%10;
+            sum=sum+(ld*ld*ld);
+            rev=(rev*10)+ld;
+            n=n/10;
+        }
+        if(sum==dup)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
