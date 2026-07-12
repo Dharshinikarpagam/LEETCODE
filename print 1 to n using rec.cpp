4961164c4892df3141cabbc8404d051a43c1707a
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-12 10:06:51 +0530

    Create print 1 to n using rec.cpp

---
+++ print 1 to n using rec.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void func(int i,int n)
+    {
+        if(i>n)
+        {
+            return;
+        }
+        cout<<i<<" ";
+        func(i+1,n);
+    }
+    void printTillN(int n) {
+       func(1,n);
+        
+    }
+};
