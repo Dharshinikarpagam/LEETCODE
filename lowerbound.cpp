255200073226f30b11053367280bd04630d93ddc
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-18 14:16:20 +0530

    Create lowerbound.cpp

---
+++ lowerbound.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        int n=arr.size();
+        int l=0;
+        int h=n-1;
+        int ans=n;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(arr[mid]>=target)
+            {
+                ans=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
