45d853b0e9d635d1c14a82d4e0ae1f0bdd2b4b8a
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-21 19:23:01 +0530

    uses bstiterator

---
+++ Two sum in BST.cpp
@@ -0,0 +1,27 @@
+class BSTIterator {
+private: stack<TreeNode*> st;
+public:
+    BSTIterator(TreeNode* root) {
+        pushall(root);
+    }
+    
+    int next() {
+        TreeNode* temp=st.top();
+        st.pop();
+        pushall(temp->right);
+        return temp->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+private: 
+    void pushall(TreeNode* node)
+    {
+        for(;node!=NULL;){
+            st.push(node);
+            node=node->left;
+        }
+    }
+};
+
