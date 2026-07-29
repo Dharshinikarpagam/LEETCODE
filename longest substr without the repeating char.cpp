5bd31b7ffe560928caa58091b86e2b56ac8e0c12
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-29 12:04:12 +0530

    Update longest substr without the repeating char.cpp

--- longest substr without the repeating char.cpp
+++ longest substr without the repeating char.cpp
@@ -28,3 +28,28 @@ public:
 
     }
 };
+
+2)
+    class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0,r=0,maxLen=0,n=s.length();
+        if(n==0) return 0;
+        if(n==1) return 1;
+        unordered_map<char,int> mpp;
+        while(r<n){
+            if(mpp[s[r]]==0){
+                mpp[s[r]]=1;
+                r++;
+                maxLen = max(maxLen, r-l);
+            }
+            else{
+                maxLen=max(maxLen,r-l);
+                mpp[s[l]]=0;
+                l++;
+            }
+        }
+        return maxLen;
+
+    }
+};
