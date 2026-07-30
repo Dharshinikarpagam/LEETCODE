08eb473d981c888463647d7361707f4912a8e9e4
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-30 21:57:57 +0530

    uses bit manipulation for solving

--- dividing 2 number without multiplication and division .cpp
+++ dividing 2 number without multiplication and division .cpp
@@ -8,9 +8,9 @@ public:
         bool sign=true;
         if(dividend>=0 && divisor<0) sign=false;
         if(dividend<0 && divisor>0) sign=false;
-        long n=abs(dividend);
-        long d=abs(divisor);
-        long ans=0;
+        long long n=llabs((long long)dividend);
+        long long d=llabs((long long)divisor);
+        long long ans=0;
         while(n>=d)
         {
             int cnt=0;
@@ -18,17 +18,17 @@ public:
             {
                 cnt++;
             }
-            ans+=1<<cnt;
+            ans+=1LL<<cnt;
             n-=d<<(cnt);
         }
-        if(ans==(1<<31) && sign)
+        if(ans==(1LL<<31) && sign)
         {
             return INT_MAX;
         }
-        if(ans==(1<<31) && !sign)
+        if(ans==(1LL<<31) && !sign)
         {
             return INT_MIN;
         }
-        return sign? ans:(-1)*ans;
+        return sign? ans:-ans;
     }
 };
