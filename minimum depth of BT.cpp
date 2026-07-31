a5afdd28222ad88a05a52d7bdc432f946f176087
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-07-31 22:17:53 +0530

    uses recursion

---
+++ minimum depth of BT.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int lh=minDepth(root->left);
+        int rh=minDepth(root->right);
+        if(root->left==NULL)
+        {
+            return 1+rh;
+        }
+        if(root->right==NULL)
+        {
+            return 1+lh;
+        }
+
+        return 1+min(lh,rh);
+    }
+};
