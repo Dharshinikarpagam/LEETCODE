bb4f3f2080a4a469e261f23901c7eea6e5f69060
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-08-07 14:25:21 +0530

    uses sliding window approach

---
+++ Sliding_Window/longest substring with k unique elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+            int n=s.size();
+            int l=0;
+            int r=0;
+            int maxlen=-1;
+            map<char,int> mpp;
+            while(r<n)
+            {
+                mpp[s[r]]++;
+                if(mpp.size()>k)
+                {
+                    mpp[s[l]]--;
+                    if(mpp[s[l]]==0)
+                    {
+                        mpp.erase(s[l]);
+                    }
+                    l++;
+                }
+                if(mpp.size()==k)
+                {
+                    maxlen=max(maxlen,r-l+1);
+                }
+                r++;
+            }
+            return maxlen;
+    }
+};
