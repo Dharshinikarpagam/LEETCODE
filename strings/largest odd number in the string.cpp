2182f381b3f1c4545b4ece0832902f1b850b186e
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-08-11 22:13:51 +0530

    Update largest odd number in the string.cpp

--- strings/largest odd number in the string.cpp
+++ strings/largest odd number in the string.cpp
@@ -11,5 +11,15 @@ public:
            }
         }
         return s;
+
+//optimal soln
+        int n=num.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if((num[i]-'0')%2!=0){
+                return num.substr(0,i+1);
+            }
+        }
+        return "";
     }
 };
