bb59019467a0d332730a3fefa69fd8b6196e359c
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-08-13 21:52:42 +0530

    uses same minpathsum concept but in traingle pattern

---
+++ DP/2D/triangle.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+//recursion
+    int path(int i,int j,vector<vector<int>> &triangle,int n)
+    {
+        if(i==n-1)
+        {
+            return triangle[n-1][j];
+        }
+        int down=triangle[i][j]+path(i+1,j,triangle,n);
+        int dia=triangle[i][j]+path(i+1,j+1,triangle,n);
+        return min(down,dia);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        return path(0,0,triangle,n);
+    }
+
+//memoization
+   int path(int i,int j,vector<vector<int>> &triangle,int n,vector<vector<int>> &dp)
+    {
+        if(i==n-1)
+        {
+            return triangle[n-1][j];
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=triangle[i][j]+path(i+1,j,triangle,n,dp);
+        int dia=triangle[i][j]+path(i+1,j+1,triangle,n,dp);
+        return dp[i][j]=min(down,dia);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return path(0,0,triangle,n,dp);
+    }
+
+//tabulation
+ int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down=triangle[i][j]+dp[i+1][j];
+                int dia=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,dia);
+            }
+        }
+        return dp[0][0];
+    }
+};
