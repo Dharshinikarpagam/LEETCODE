977c3babe201e3ea979aeb2e372977447d47843e
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-09-02 12:09:32 +0530

    Update kth smallest ele in BST.cpp

--- kth smallest ele in BST.cpp
+++ kth smallest ele in BST.cpp
@@ -35,4 +35,25 @@ public:
         }
      return ans;
     }
+
+//next soln
+class Solution {
+public:
+    int ans=-1;
+    int cnt=0;
+    void inorder(TreeNode* root,int k){
+        if(!root) return;
+        inorder(root->left,k);
+        cnt++;
+        if(cnt==k) {
+            ans=root->val;
+            return;
+        }
+        inorder(root->right,k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root,k);
+        return ans;
+    }
 };
