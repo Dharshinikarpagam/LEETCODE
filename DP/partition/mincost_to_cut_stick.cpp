f4a6022c5570ebb969f70e65a4e7e2ba63a77251
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-09-09 21:29:18 +0530

    Create mincost_to_cut_stick.cpp

---
+++ DP/partition/mincost_to_cut_stick.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+//recursion
+    int f(int i,int j,vector<int>& cuts)
+    {
+        int mini=INT_MAX;
+        if(i>j) return 0;
+        for(int ind=i;ind<=j;ind++)
+        {
+            int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts)+f(ind+1,j,cuts);
+            mini=min(mini,cost);
+        }
+        return mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        int m=cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        return f(1,m,cuts);
+    }
+//memoization
+
+int f(int i,int j,vector<int>& cuts,vector<vector<int>>& dp)
+    {
+        int mini=INT_MAX;
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        for(int ind=i;ind<=j;ind++)
+        {
+            int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
+            mini=min(mini,cost);
+        }
+        return dp[i][j]=mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        int m=cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
+        return f(1,m,cuts,dp);
+    }
+
+//tabulation
+int minCost(int n, vector<int>& cuts) {
+        int m=cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>> dp(m+2,vector<int>(m+2,0));
+        for(int i=m;i>=1;i--)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(i>j) continue;
+                int mini=INT_MAX;
+                for(int ind=i;ind<=j;ind++)
+                {
+                    int cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][m];
+    }
+};
