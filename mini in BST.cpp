4a9d7f2ceb0c268f7d97b83a648175e2dec4d284
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-09-16 21:43:57 +0530

    Create mini in BST.cpp

---
+++ mini in BST.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int minValue(Node* root) {
+      while(root->left!=NULL){
+          root=root->left;
+      }
+      return root->data;
+        
+        
+    }
+};
